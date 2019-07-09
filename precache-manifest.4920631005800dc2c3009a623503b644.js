cd275e7d605ced7522f52b29c3dafffc6e2162fd
Simanta <syed.simanta06@gmail.com>
2019-07-09 22:43:09 +0600

    Updates

---
+++ precache-manifest.4920631005800dc2c3009a623503b644.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2186ff6e66cd0e6ea2ebb64c3c23832e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e6a3ea4dc2a6b8a01a3",
+    "url": "/static/css/main.ea053062.chunk.css"
+  },
+  {
+    "revision": "811c824a6d720b576ab8",
+    "url": "/static/js/2.97d815d9.chunk.js"
+  },
+  {
+    "revision": "2e6a3ea4dc2a6b8a01a3",
+    "url": "/static/js/main.58de88ef.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4ddc758d490102bf391845e6f0791725",
+    "url": "/static/media/background.4ddc758d.jpg"
+  }
+]);
